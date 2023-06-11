b04f97ac8c789725f1f41d267ba3376eeeac59a3
softfietea <135832063+alfie-tribaco@users.noreply.github.com>
2023-06-11 15:40:45 +0800

    initial computation

---
+++ num_differentiation_app/lib/features/computation/screens/computation_screen.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+class ComputationScreen extends StatefulWidget {
+  const ComputationScreen({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<ComputationScreen> createState() => _ComputationScreenState();
+}
+
+class _ComputationScreenState extends State<ComputationScreen> {
+  TextEditingController functionController = TextEditingController();
+  TextEditingController pointController = TextEditingController();
+  TextEditingController stepSizeController = TextEditingController();
+  String result = '';
+  int _counter = 0;
+
+  void computeDerivatives() {
+    String function = functionController.text;
+    double? point = double.tryParse(pointController.text);
+    double? stepSize = double.tryParse(stepSizeController.text);
+
+    if (function.isEmpty || point == null || stepSize == null) {
+      setState(() {
+        result = 'Invalid input';
+      });
+      return;
+    }
+
+    Parser parser = Parser();
+    Expression expression = parser.parse(function);
+
+    // Create a context to hold the variable values
+    ContextModel context = ContextModel();
+    context.bindVariable(Variable('x'), Number(point));
+    setState(() {
+      double forwardDifference =
+          computeForwardDifference(expression, point, stepSize);
+      double backwardDifference =
+          computeBackwardDifference(expression, point, stepSize);
+      double centralDifference =
+          computeCentralDifference(expression, point, stepSize);
+
+      print("FORWARD: " + forwardDifference.toString());
+
+      result =
+          'Forward difference: $forwardDifference\nBackward difference: $backwardDifference\nCentral difference: $centralDifference';
+    });
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Please Input'),
+            TextField(
+              controller: functionController,
+              onChanged: (e) {},
+            ),
+            TextField(
+              controller: pointController,
+              onChanged: (e) {},
+            ),
+            TextField(
+              controller: stepSizeController,
+            ),
+            Text(
+              result,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          computeDerivatives();
+        },
+        tooltip: 'Compute',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+double computeForwardDifference(Expression expression, double x, double h) {
+  ContextModel context = ContextModel();
+  context.bindVariable(Variable('x'), Number(x));
+
+  double f = expression.evaluate(EvaluationType.REAL, context);
+
+  context.bindVariable(Variable('x'), Number(x + h));
+  double fPlusH = expression.evaluate(EvaluationType.REAL, context);
+
+  double derivative = (fPlusH - f) / h;
+
+  return derivative;
+}
+
+double computeBackwardDifference(Expression expression, double x, double h) {
+  ContextModel context = ContextModel();
+  context.bindVariable(Variable('x'), Number(x));
+
+  double f = expression.evaluate(EvaluationType.REAL, context);
+
+  context.bindVariable(Variable('x'), Number(x - h));
+  double fMinusH = expression.evaluate(EvaluationType.REAL, context);
+
+  double derivative = (f - fMinusH) / h;
+
+  return derivative;
+}
+
+double computeCentralDifference(Expression expression, double x, double h) {
+  ContextModel context = ContextModel();
+  context.bindVariable(Variable('x'), Number(x - h));
+  double fMinusH = expression.evaluate(EvaluationType.REAL, context);
+
+  context.bindVariable(Variable('x'), Number(x + h));
+  double fPlusH = expression.evaluate(EvaluationType.REAL, context);
+
+  double derivative = (fPlusH - fMinusH) / (2 * h);
+
+  return derivative;
+}
